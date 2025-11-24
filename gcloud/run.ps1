a0461c62f86d9fbb3a3d82a9ff72d65344925978
David Liu <david-khala@hotmail.com>
2025-11-24 09:56:52 +0800

    backup from office

--- gcloud/run.ps1
+++ gcloud/run.ps1
@@ -1,10 +1,16 @@
 # 定义变量
 $PROJECT_ID = "freetier-only"
-$image = "gcr.io/$PROJECT_ID/dynamic-webapp"
+$REGION = "asia-east2"
+$REPO = "cloud-run-source-deploy"
+$IMAGE_NAME = "dynamic-webapp"
 $project_root = "../"
 
+# 镜像路径改为 Artifact Registry 格式
+$image = "$REGION-docker.pkg.dev/$PROJECT_ID/$REPO/$IMAGE_NAME"
+
+# 构建并推送镜像到 Artifact Registry
 gcloud builds submit $project_root --tag $image
 
+# FIXME permission issue
 # 部署到 Cloud Run (台湾区域 asia-east2)
-gcloud run deploy dynamic-webapp --image $image --platform managed --allow-unauthenticated `
-    --region asia-east2
+gcloud run deploy $IMAGE_NAME --image $image --platform managed --allow-unauthenticated --region $REGION
