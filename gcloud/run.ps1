c79818385fb9c2f8c4737c69d96b777cb01b629a
David Liu <david-khala@hotmail.com>
2025-11-27 08:34:02 +0800

    done

--- gcloud/run.ps1
+++ gcloud/run.ps1
@@ -1,16 +1,13 @@
-# 定义变量
 $PROJECT_ID = "freetier-only"
-$REGION = "asia-east2"
+$REGION = "asia-east2" # HK
 $REPO = "cloud-run-source-deploy"
 $IMAGE_NAME = "dynamic-webapp"
 $project_root = "../"
-
-# 镜像路径改为 Artifact Registry 格式
+$gserviceaccount = "cloud-region@freetier-only.iam.gserviceaccount.com"
 $image = "$REGION-docker.pkg.dev/$PROJECT_ID/$REPO/$IMAGE_NAME"
 
-# 构建并推送镜像到 Artifact Registry
-gcloud builds submit $project_root --tag $image
+gcloud iam service-accounts add-iam-policy-binding $gserviceaccount --member="user:davidkhala@gmail.com" --role="roles/iam.serviceAccountUser" --project=$PROJECT_ID
 
-# FIXME permission issue
-# 部署到 Cloud Run (台湾区域 asia-east2)
-gcloud run deploy $IMAGE_NAME --image $image --platform managed --allow-unauthenticated --region $REGION
+gcloud builds submit $project_root --tag $image --region $REGION --project=$PROJECT_ID
+
+gcloud run deploy $IMAGE_NAME --image $image --service-account=$gserviceaccount --platform managed --allow-unauthenticated --region $REGION --project=$PROJECT_ID
--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Body
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+
 app = FastAPI()
 
-# 用字典来存储动态页面 {url_path: html_content}
 dynamic_pages = {}
 
+
+class PageContent(BaseModel):
+    content: str
+
+
 @app.post("/{page_name}")
-def create_page(page_name: str, content: str):
+def create_page(page_name: str,
+                _json: PageContent | None = Body(None),
+                ):
     if page_name in dynamic_pages:
         raise HTTPException(status_code=400, detail="Page already exists")
-    dynamic_pages[page_name] = content
-    return {"message": f"Page '{page_name}' created", "url": f"/{page_name}"}
+    dynamic_pages[page_name] = _json.content
+    return {"message": f"Page '{page_name}' created"}
+
 
 @app.get("/{page_name}", response_class=HTMLResponse)
 def get_page(page_name: str):
     if page_name not in dynamic_pages:
         raise HTTPException(status_code=404, detail="Page not found")
     return dynamic_pages[page_name]
+
 
 @app.delete("/{page_name}")
 def delete_page(page_name: str):
